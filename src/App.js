87f79ce7326f1a2ded167718052b334bcb5e1557
Thekolorz <73794752+Thekolorz@users.noreply.github.com>
2021-03-06 23:34:46 +0100

    fixed the problem with help of Adrian products equals to products

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ const App = () => {
     
     <div>
       <Navbar />
-      <Products product={products}/>
+      <Products products={products}/>
     </div>
   )
 }
--- src/chec-commerce.ts
+++
@@ -1,1 +0,0 @@
-declare module '@chec/commerce';
