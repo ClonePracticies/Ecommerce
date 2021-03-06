6e2ad8da823eae5273d81346b2af95974d62483c
Thekolorz <73794752+Thekolorz@users.noreply.github.com>
2021-03-06 20:30:50 +0100

    edited navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -14,7 +14,7 @@ import AccountCircle from '@material-ui/icons/AccountCircle';
 import MailIcon from '@material-ui/icons/Mail';
 import NotificationsIcon from '@material-ui/icons/Notifications';
 import MoreIcon from '@material-ui/icons/MoreVert';
-import {AddShoppingCart, ShoppingCart} from '@material-ui/icons';
+import { ShoppingCart} from '@material-ui/icons';
 
 import useStyles from './styles';
 
